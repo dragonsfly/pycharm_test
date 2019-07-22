7c20ec81161afc7e0c09226eec036645d055a483
dragonsfly <xinglong_work@163.com>
2019-07-22 12:11:03 +0800

    第2次提交

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -17,10 +17,11 @@ class TestLogin:
         assert 1
 
     def test_login4(self):
-        assert 1
+        assert 0
 
     def test_login5(self):
         assert 1
 
     def test_login6(self):
         assert 0
+
